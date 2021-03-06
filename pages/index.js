1a55c049c2a984049007312bdad4b8fa06e75b49
OnceLearner <redaamir972@gmail.com>
2021-03-06 01:30:40 +0100

    just a test

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from "next/image"
 import Amenities from '../components/Amenities'
 import FooterHigh from '../components/FooterHigh'
 import FooterLow from '../components/FooterLow'
@@ -19,6 +20,53 @@ export default function Home() {
       </nav>
 
       <main className={styles.main}>
+        <div className="flex flex-col  space-y-2 p-7 md:px-12">
+          <h2 className="text-2xl font-semibold mb-2 md:mb-7 text-gray-800">
+            DUNNWOOD GREEN-LUXURY APARTMENTS
+          </h2>
+
+          <p className="text-gray-600  leading-6">
+            Morrell Builders is thrilled to announce our newest project! Dunnwood Green will provide the same quality, value and innovation Rochester home buyers have revered for three generations. Three luxury buildings offering single and multiple bedroom residences surrounded by a golf course and wooded trails.  A community club house featuring your favorite coffee bar, a beloved pub, a comfy library, and a state-of-the-art fitness center. Our goal is to energize and elevate your lifestyle.
+          </p>
+          <p className="text-gray-600  py-2  ">
+            As you learn more, we hope you will become equally as excited, so stay tuned and we will provide additional details in the coming months!
+          </p>
+        </div>
+        <div className="  w-full      ">
+          <div className="  relative   w-full  h-72 md:h-full        ">
+            <Image
+              className="  "
+              alt="Mountains"
+              src="/mainimg.jpg"
+              layout="fill"
+              objectFit="cover"
+              quality={100}
+            />
+          </div>
+          <div className="  relative md:hidden   w-full h-72 md:h-full        ">
+            <Image
+              className="  "
+              alt="Mountains"
+              src="/mainimg.jpg"
+              layout="fill"
+              objectFit="cover"
+              quality={100}
+            />
+          </div>
+          <div className="  relative md:hidden   w-full h-72 md:h-full  mb-16     ">
+            <Image
+              className="  "
+              alt="Mountains"
+              src="/mainimg.jpg"
+              layout="fill"
+              objectFit="cover"
+              quality={100}
+            />
+          </div>
+
+
+        </div>
+
         <Amenities />
       </main>
 
