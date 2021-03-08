989ee1613c644079f4c25a7bfb648d23340b8939
OnceLearner <redaamir972@gmail.com>
2021-03-08 20:45:59 +0100

    ne afix

--- components/Gallery.js
+++ components/Gallery.js
@@ -10,17 +10,22 @@ const Gallery = () => {
 
     return (
         <div className=" w-screen h-screen relative flex flex-col transform     mt-20">
-            <Image src={`/gallery/${choosedImg}`} layout="fill" objectFit="cover" quality={1} priority />
+            {
+                titlesImgs.map(tm =>
+
+                    <Image src={`/gallery/${choosedImg}`} className={`${choosedImg == tm.imgName ? '' : 'hidden'}`} layout="fill" objectFit="cover" quality={75} priority />
+                )
+            }
 
 
+            <div className="flex flex-col absolute top-0 cursor-pointer    space-y-11 pt-10 p-10  h-full ">
+                {titlesImgs.map(tm =>
+                    <h2 className="text-7xl text-gray-100 font-extrabold hover:text-purple-500" onMouseOver={() => setChoosedImg(tm.imgName)}>  {tm.title} </h2>
 
-            <div className="flex flex-col absolute top-0 cursor-pointer   space-y-11 pt-10 p-10  h-full ">
-                {
-                    titlesImgs.map(tm =>
-                        <h2 className="text-7xl text-gray-100 font-extrabold hover:text-purple-500" onMouseOver={() => setChoosedImg(tm.imgName)}>  {tm.title} </h2>
-                    )
+                )
                 }
             </div>
+
 
 
 
