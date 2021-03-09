6806bae3f325d71d59bdd027de0cb2a8dfeeb5f3
OnceLearner <redaamir972@gmail.com>
2021-03-09 01:55:39 +0100

    new commit '

--- components/Gallery.js
+++ components/Gallery.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Image from "next/image"
 import { titlesImgs } from './data/Gallery'
 
+import styles from '../styles/Home.module.css'
+
+
 
 
 const Gallery = () => {
@@ -9,18 +12,21 @@ const Gallery = () => {
     const [choosedImg, setChoosedImg] = React.useState(titlesImgs[0].imgName)
 
     return (
-        <div className=" w-screen h-screen relative flex flex-col transform     mt-20">
+        <div className=" w-full h-screen relative flex flex-col    mt-20" style={{}}>
+
+
+
             {
                 titlesImgs.map(tm =>
 
-                    <Image src={`/gallery/${choosedImg}`} className={`${choosedImg == tm.imgName ? '' : 'hidden'}`} layout="fill" objectFit="cover" quality={75} priority />
+                    <Image style src={`/gallery/${choosedImg}`} className={`${tm.imgName === choosedImg ? styles.show : styles.iconContainer}`} layout="fill" objectFit="cover" quality={75} priority />
+
                 )
             }
-
-
-            <div className="flex flex-col absolute top-0 cursor-pointer    space-y-11 pt-10 p-10  h-full ">
+            <div className="flex flex-col absolute top-0 cursor-pointer  justify-evenly    pt-10 p-10  h-full ">
                 {titlesImgs.map(tm =>
-                    <h2 className="text-7xl text-gray-100 font-extrabold hover:text-purple-500" onMouseOver={() => setChoosedImg(tm.imgName)}>  {tm.title} </h2>
+
+                    <h2 className="md:text-6xl text-xl text-gray-100 font-extrabold  box-content transition ease-in duration-150 hover:text-green-500" onMouseOver={() => setChoosedImg(tm.imgName)} onClick={() => setChoosedImg(tm.imgName)}>  {tm.title} </h2>
 
                 )
                 }
--- components/data/Gallery.js
+++ components/data/Gallery.js
@@ -2,8 +2,17 @@
 //  all images will go under the /public/gallery/   folder ,
 
 export const titlesImgs = [
-    { title: "Miami", imgName: "miami.jpg" },
-    { title: "Maldives", imgName: "maldives.jpg" },
-    { title: "under Sea", imgName: "underSea.jpg" }
+    { title: "Pub", imgName: "Pub.jpg" },
+    { title: "Coffee Bar", imgName: "Coffee Bar.jpg" },
+    { title: "Bird's eye of Dunnwood", imgName: "Bird's eye of Dunnwood.jpg" },
+    { title: "Pool", imgName: "Pool.png" },
+    { title: "Whiskey Lockers", imgName: "Whiskey Lockers.jpg" },
+    { title: "Apartment Lobby ", imgName: "Apartment Lobby.jpg" },
+    { title: "Apartment Lobby 2", imgName: "Apartment Lobby 2.jpg" },
+    { title: "Club Room", imgName: "Club Room.jpg" },
+
+
+
+
 ]
 
